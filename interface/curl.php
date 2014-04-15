29698b430e7de72d12db12ebf1f8c2e82c8583dc
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-04-15 11:27:25 +0200

    cleanup

--- interface/curl.php
+++ interface/curl.php
@@ -3,21 +3,18 @@
   $command = $_GET['command'];
 
   // Create cURL call
-  $service_url = 'http://arduino.local/'. $command;
+  $service_url = 'http://arduino.local'. $command;
   $curl = curl_init($service_url);
    
   // Send cURL to Yun board
   curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 ); 
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,1);
+  //curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  //curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,1);
   $curl_response = curl_exec($curl);
   curl_close($curl);
 
   //Print answer
-  if ($curl_response != ""){
-    echo $curl_response;  
-  }
-  else {
+  if ($curl_response == ""){
     echo "{\"connected\": false}";
   }
 
